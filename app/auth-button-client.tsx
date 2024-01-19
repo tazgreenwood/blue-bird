63dd996146fcdf89b71056b03a533a836054b8b0
Taz Greenwood <tazgreenwood@Tazs-MacBook-Pro-2.local>
2024-01-19 14:43:18 -0700

    deploy app

---
+++ app/auth-button-client.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { createClientComponentClient, Session } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+
+export default function AuthButtonClient({ session }: { session: Session | null }) {
+    const supabase = createClientComponentClient<Database>()
+    const router = useRouter();
+
+    const handleSignOut = async () => {
+        await supabase.auth.signOut();
+        router.refresh();
+    }
+    const handleSignIn = async () => {
+        supabase.auth.signInWithOAuth({
+            provider: 'github',
+            options: {
+                redirectTo: `${location.origin}/auth/callback`
+            }
+        })
+    }
+
+    return session ? (
+        <button className="text-xs text-gray-400" onClick={handleSignOut}>Logout</button>
+    ) : (
+        <button className="text-xs text-gray-400" onClick={handleSignIn}>Login</button>
+    );
+
+}
